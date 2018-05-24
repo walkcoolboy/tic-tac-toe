723a60f433dfc6bc40cabc4e90fb27387da7539c
Li Li <walkcoolboyli@gmail.com>
2018-05-24 18:41:20 +1200

    Add CSS grid board with styled-components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { Board } from '..'
+
+const StyledApp = styled.div`
+  display: grid;
+  font-family: 'Verdana', sans-serif;
+  grid-template-areas: 'board';
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+`
+
+StyledApp.displayName = 'StyledApp'
 
 export default function App () {
-  return <h1>Tic-Tac-Toe</h1>
+  return (
+    <StyledApp>
+      <Board>
+        <div>0</div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </Board>
+    </StyledApp>
+  )
 }
