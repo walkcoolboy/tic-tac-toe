31ae9b3c6acd71a9c6cdd27c1e944291bf4d5cec
Li Li <walkcoolboyli@gmail.com>
2018-05-24 19:04:57 +1200

    used ramda for looping 9 squares

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import { times } from 'ramda'
 
-import { Board } from '..'
+import { Board, Square } from '..'
+
+const makeSquares = () =>
+  times(
+    idx => <Square key={idx} index={idx} player={idx % 2 === 0 ? 'x' : 'o'} />,
+    9
+  )
 
 const StyledApp = styled.div`
   display: grid;
@@ -12,22 +19,13 @@ const StyledApp = styled.div`
   padding: 0;
   width: 100vw;
 `
-
 StyledApp.displayName = 'StyledApp'
 
 export default function App () {
   return (
     <StyledApp>
       <Board>
-        <div>0</div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
+        {makeSquares()}
       </Board>
     </StyledApp>
   )
